b294a9ee2a2d7e4d9dbe7d6b79c264e05a5ac4f7
Millylimis <dunamildred@gmail.com>
2024-08-27 19:45:32 +0100

    Update Erc20.sol

--- contracts/Erc20.sol
+++ contracts/Erc20.sol
@@ -1,18 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-contract ERC20 {
+contract myTokens {
     uint public totalSupply;
+    address owner= msg.sender;
+
     mapping(address => uint) public balanceOf;
     mapping(address => mapping(address => uint)) public allowance;
-    string public name = "Solidity by Example";
-    string public symbol = "SOLBYEX";
+
+    string public name = "myTokens";
+    string public symbol = "MTS";
     uint8 public decimals = 18;
 
-    event Transfer(address indexed from, address indexed to, uint value);
+	event Transfer(address indexed from, address indexed to, uint value);
     event Approval(address indexed owner, address indexed spender, uint value);
 
     function transfer(address recipient, uint amount) external returns (bool) {
+        require(balanceOf[msg.sender] >= amount, "Insufficent Balance");
         balanceOf[msg.sender] -= amount;
         balanceOf[recipient] += amount;
         emit Transfer(msg.sender, recipient, amount);
@@ -29,7 +33,7 @@ contract ERC20 {
         address sender,
         address recipient,
         uint amount
-    ) external returns (bool) {
+    ) external returns (bool) {  
         allowance[sender][msg.sender] -= amount;
         balanceOf[sender] -= amount;
         balanceOf[recipient] += amount;
@@ -38,12 +42,14 @@ contract ERC20 {
     }
 
     function mint(uint amount) external {
+        require(msg.sender == owner, "you are not the owner");
         balanceOf[msg.sender] += amount;
         totalSupply += amount;
         emit Transfer(address(0), msg.sender, amount);
     }
 
     function burn(uint amount) external {
+        require(balanceOf[msg.sender] >= amount, "Insufficent balance");
         balanceOf[msg.sender] -= amount;
         totalSupply -= amount;
         emit Transfer(msg.sender, address(0), amount);
